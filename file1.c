661a897a2b2384a5c53766f5c9574e85564c06dd
Tsddcslf <tsddcslf@gmail.com>
2023-11-05 21:48:57 +0800

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("here\n");
+//
+//	return 0;
+//}
+
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int  a = 10;
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	long long num1 = 0;
+	long long num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	long long sum = num1 + num2;
+	printf("%d\n", sum);
+
+	return 0;
+}
