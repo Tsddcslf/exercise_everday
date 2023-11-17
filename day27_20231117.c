cff1fe49a0acc9292b0ef10321c22a190ff34aa6
Tsddcslf <tsddcslf@gmail.com>
2023-11-18 00:04:20 +0800

    Create day27_20231117.c

---
+++ day27_20231117.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+//int main()
+//{
+//    int a;
+//    while(scanf("%x",&a)!=EOF)
+//    {
+//        printf("%d\n",a);
+//    }
+//    return 0;
+//}
+
+//%e，将浮点数以科学计数法的方式表示出来。
+//int main()
+//{
+//    double a=5.2;
+//    printf("%e",a);
+//    return 0;
+//}
+
+////调试，debug
+//int main()
+//{
+//    int i=0;
+//    int arr[10];
+//    for(int i=0;i<10;i++)
+//    {
+//        arr[i]=i;
+//    }
+//    for(i=0;i<10;i++)
+//    {
+//        printf("%d",i);
+//    }
+//    return 0;
+//}
+
+
+//int add(int x,int y)
+//{
+//    return x+y;
+//}
+//
+//int main()
+//{
+//    int a=10;
+//    int b=20;
+//    int c=add(a,b);
+//    printf("%d",c);
+//    return 0;
+//}
+
+//void test(int a[])
+//{
+//    //
+//}
+//int main()
+//{
+//    int arr[10]={0,1,2,3,4,5,6,7,8,9};
+//    test(arr);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n;
+//    while(scanf("%d",&n)!=EOF)
+//   {
+//       int sum=0;
+//       for(int i=1;i<=n;i++)
+//       {
+//           int sum0=1;
+//           for(int j=1;j<=i;j++)
+//           {
+//               sum0=sum0*j;
+//           }
+//           sum+=sum0;
+//       }
+//       printf("%d\n",sum);
+//   }
+//    return 0;
+//}
+
+int main()
+{
+    int arr[10]={0};
+    int i=0;
+    for(i=0;i<=12;i++)
+    {
+        arr[i]=0;
+        printf("hehe\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
